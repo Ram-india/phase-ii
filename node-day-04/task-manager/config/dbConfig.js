661faac2ce12d51387a3c406eb82c60d3f3b3193
ram <bvn.ramkumar@gmail.com>
2025-04-07 22:52:27 +0530

    Put and patch methods are updated

--- node-day-04/task-manager/config/dbConfig.js
+++ node-day-04/task-manager/config/dbConfig.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async() =>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/taskManager");
+        await mongoose.connect(
+            process.env.MONGO_URI
+            );
         console.log("Connected to MongoDB");
     }catch(error){
         console.log("Database conection faild");
--- node-day-04/task-manager/server.js
+++ node-day-04/task-manager/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/dbConfig');
 const taskRoutes = require("./routes/taskRoutes");
+require("dotenv").config();
 
 const app = express();
 
@@ -15,7 +16,7 @@ connectDB();
 // Routes
 app.use("/api/tasks", taskRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT }`);
 });
